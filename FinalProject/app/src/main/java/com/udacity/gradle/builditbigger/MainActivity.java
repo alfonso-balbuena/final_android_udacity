d28a51579d71a59c6ff28874c924734c6ca9f99e
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2020-10-25 19:44:06 -0600

    Added android library

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.alfonso.jokeactivity.JokeActivity;
 import com.alfonso.jokeprovider.Provider;
 
 
@@ -45,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-
-        Toast.makeText(this, jokeProvider.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,JokeActivity.class);
+        intent.putExtra(JokeActivity.JOKE_PARAMETER,jokeProvider.getJoke());
+        startActivity(intent);
     }
 
 
